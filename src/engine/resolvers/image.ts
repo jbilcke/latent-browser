21a43ee523d9b1bcafb9c83a78d0cabaaf06db8a
Julian Bilcke <julian.bilcke@gmail.com>
2022-12-31 14:40:55 +0100

    safer image replacement

--- src/engine/resolvers/image.ts
+++ src/engine/resolvers/image.ts
@@ -19,21 +19,25 @@ export async function resolveImages() {
     console.log('found image:', { alt, src })
     if (alt) {
       console.log('found image prompt:', alt)
-      images[i].src = spinner.src
-      // images[i].width = 300
-      // images[i].height = 300
-      // TODO: adapt the resolution to the image width/height
-      // we could do one pass at low resolution, then use it as an img-to-img input for a second pass
-      // although this is a waste of quota
+      try {
+        images[i].src = spinner.src
+        // images[i].width = 300
+        // images[i].height = 300
+        // TODO: adapt the resolution to the image width/height
+        // we could do one pass at low resolution, then use it as an img-to-img input for a second pass
+        // although this is a waste of quota
 
-      const response = await fetch(
-        `/api/image?prompt=${encodeURIComponent(alt)}`
-      )
-      const { url, prompt, width, height } =
-        (await response.json()) as DalleImage
+        const response = await fetch(
+          `/api/image?prompt=${encodeURIComponent(alt)}`
+        )
+        const { url, prompt, width, height } =
+          (await response.json()) as DalleImage
 
-      console.log('url', url)
-      images[i].src = url
+        console.log('url', url)
+        images[i].src = url
+      } catch (err) {
+        console.log('failed to replace an image:', err)
+      }
     }
   }
 
